9c7698fdfdeb0b5458da912422453d99b1c161a3
Rehman47 <rehmanshaik324@gmail.com>
2026-04-09 02:46:28 +0530

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Rehman"
+let age = 21
+let location;
+let marks = null
+
+
+
+/* Primitive DataTypes =>
+number => 2 pwr of 53(size of number), and any number like 23,3.14,0.5 is a type number
+bigint => larger than number
+string => "", any text wraped in quotes
+boolean => true/false only two possible values
+null => standalone value and a type of object
+undefined => decalred but value not assigned
+*/
+
+
+console.log(typeof location); // returns the type of the varible
+console.log(typeof marks);
+
